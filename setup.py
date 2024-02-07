3b3064ded8132efe035f5494e59354ab59c52cb5
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2024-02-07 12:52:38 +0800

    alvistack/0.11.8
        git clean -xdf
        tar zcvf ../python-tomlkit_0.11.8.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-tomlkit.spec ../python-tomlkit_0.11.8-1.spec
        cp ../python*-tomlkit*0.11.8*.{gz,xz,spec,dsc} /osc/home\:alvistack/sdispater-tomlkit-0.11.8/
        rm -rf ../python*-tomlkit*0.11.8*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tomlkit']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'tomlkit',
+    'version': '0.11.8',
+    'description': 'Style preserving TOML library',
+    'author': 'Sébastien Eustace',
+    'author_email': 'sebastien@eustace.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/sdispater/tomlkit',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
